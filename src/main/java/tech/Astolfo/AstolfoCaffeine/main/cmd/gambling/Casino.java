4ee8d65d767c605849a970606efc4b15236d9e9f
owouwuowo <digitalgpro@gmail.com>
2020-09-29 19:20:03 +0100

    Replaced e#reply with Logging#send and removed unnecessary declarations
    Signed-off-by: owouwuowo <digitalgpro@gmail.com>

--- src/main/java/tech/Astolfo/AstolfoCaffeine/main/cmd/gambling/Casino.java
+++ src/main/java/tech/Astolfo/AstolfoCaffeine/main/cmd/gambling/Casino.java
@@ -2,10 +2,8 @@ package tech.Astolfo.AstolfoCaffeine.main.cmd.gambling;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
-
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.User;
-import tech.Astolfo.AstolfoCaffeine.App;
+import tech.Astolfo.AstolfoCaffeine.main.msg.Logging;
 
 public class Casino extends Command {
 
@@ -19,14 +17,12 @@ public class Casino extends Command {
     @Override
     protected void execute(CommandEvent e) {
         Message msg = e.getMessage();
-        User author = msg.getAuthor();
-
-        e.reply(
-            App.embed()
-                .setAuthor("Astolfo Casino", "https://astolfo.tech", author.getAvatarUrl())
-                .setThumbnail("https://cdn.discordapp.com/attachments/738514936338055178/747059545154650122/Screenshot_2020-08-23_at_12.46.40.png")
-                .setDescription("Currently under construction, check back later!")
-                .build()
+        new Logging().send(
+                msg,
+                "Currently under construction, check back later!",
+                "Astolfo Casino",
+                "https://cdn.discordapp.com/attachments/738514936338055178/747059545154650122/Screenshot_2020-08-23_at_12.46.40.png",
+                (String[]) null
         );
     }
 }
