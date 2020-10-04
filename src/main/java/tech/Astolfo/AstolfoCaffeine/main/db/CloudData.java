b1aaf9e23210c562938d8f1abe3f7c31b75c9bc8
Znunu <61394004+Znunu@users.noreply.github.com>
2020-10-04 21:22:42 +0200

    changes

--- src/main/java/tech/Astolfo/AstolfoCaffeine/main/db/CloudData.java
+++ src/main/java/tech/Astolfo/AstolfoCaffeine/main/db/CloudData.java
@@ -1,5 +1,6 @@
 package tech.Astolfo.AstolfoCaffeine.main.db;
 
+import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import tech.Astolfo.AstolfoCaffeine.App;
@@ -18,63 +19,51 @@ public class CloudData {
         Document query_data = App.db.getCollection(collection).find(filter).first();
 
         // Check if the query returns no results
-        if (query_data == null) {
-            // Creates and insert a new default document into the collection then returns the value of the document
-            query_data = create_template(id, collection);
-        }
+        // Creates and insert a new default document into the collection then returns the value of the document
+        if (query_data == null) query_data = create_template(id, collection);
 
         // The method returns the value of the document
         return query_data;
     }
 
     // Method for creating and inserting standard documents into the MongoDB database
-    private Document create_template(long id, String collection) {
+    private Document create_template(long id, String selected_collection) {
+
+        Document to_insert = null;
 
         // Check the value of the collection variable
-        switch(collection) {
-
-            default:
-                return null;
+        switch(selected_collection) {
 
             case "wallets":
                 // Create a default wallet document
-                Document wallet = new Document("userID", id)
+                to_insert = new Document("userID", id)
                         .append("credits", 0D)
                         .append("trapcoins", 0D)
                         .append("tokens", 0D);
-
-                // Insert the wallet document into the database
-                App.db.getCollection(collection).insertOne(wallet);
-
-                // Return the default wallet document
-                return wallet;
+                break;
 
             case "stocks":
                 // Create a default stocks document
-                Document stocks = new Document("userID", id)
+                to_insert = new Document("userID", id)
                         .append("astf", 0)
                         .append("gudk", 0)
                         .append("weeb", 0)
                         .append("wolf", 0)
                         .append("emo", 0)
                         .append("vimx", 0);
-
-                // Insert the stocks document into the database
-                App.db.getCollection(collection).insertOne(stocks);
-
-                // Return the stocks document
-                return stocks;
+                break;
 
             case "tools":
                 // Create a default tools document
-                Document tools = new Document("userID", id)
+                to_insert = new Document("userID", id)
                         .append("tools", 0);
+                break;
 
-                // Insert the tools document into the database
-                App.db.getCollection(collection).insertOne(tools);
-
-                // Return the tools document
-                return tools;
         }
+
+        if (to_insert == null) throw new RuntimeException();
+        MongoCollection<Document> collection = App.db.getCollection(selected_collection);
+        collection.insertOne(to_insert);
+        return to_insert;
     }
 }
