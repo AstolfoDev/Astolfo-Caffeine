8511c5d1ec993d9a8681dca5ca6723b6a22a6aa7
owouwuowo <digitalgpro@gmail.com>
2020-10-14 22:44:40 +0100

    🔨 Added "tool_id" map to contain values for each emote id

--- src/main/java/tech/Astolfo/AstolfoCaffeine/main/util/minecraft/Toolbox.java
+++ src/main/java/tech/Astolfo/AstolfoCaffeine/main/util/minecraft/Toolbox.java
@@ -2,12 +2,47 @@ package tech.Astolfo.AstolfoCaffeine.main.util.minecraft;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageReaction;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Toolbox {
 
+    enum type {
+        SWORD,
+        PICK
+    }
+
+    public Map tool_id = new HashMap() {{
+        put(
+                type.SWORD,
+                new HashMap() {{
+                    put(0, "762750435902881875"); // Wood
+                    put(1, "762750401807122433"); // Stone
+                    put(2, "762748274435162112"); // Iron
+                    put(3, "762751771813609513"); // Iron (Enchanted)
+                    put(4, "762748240599842816"); // Gold
+                    put(5, "762751733419474974"); // Gold (Enchanted)
+                    put(6, "762746914823209010"); // Diamond
+                    put(7, "762750036160413697"); // Diamond (Enchanted)
+                    put(8, "762745406353833995"); // Netherite
+                    put(9, "762745409885175828"); // Netherite (Enchanted)
+                }}
+        );
+        put(
+                type.PICK,
+                new HashMap() {{
+                    put(0, "762750435873390622"); // Wood
+                    put(1, "762750401840939018"); // Stone
+                    put(2, "762748274246942740"); // Iron
+                    put(3, "762751771244232774"); // Iron (Enchanted)
+                    put(4, "762748240315547689"); // Gold
+                    put(5, "762751733066891314"); // Gold (Enchanted)
+                    put(6, "737855791171895308"); // Diamond
+                    put(7, "762750341376245811"); // Diamond (Enchanted)
+                    put(8, "762745405746053181"); // Netherite
+                    put(9, "762745408966361171"); // Netherite (Enchanted)
+                }}
+        );
+    }};
 
     public List<Tool> tools;
 
