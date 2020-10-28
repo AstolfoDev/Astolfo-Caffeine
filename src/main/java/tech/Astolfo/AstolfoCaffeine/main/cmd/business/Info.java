c302f6ca9f362e2798b35f8e5ccfeea0ac75e1fd
owouwuowo <digitalgpro@gmail.com>
2020-10-28 17:00:42 +0000

    🔨 Updated to use the more secure JDA#retrieveUserByID method rather than the JDA#getUserByID method which relies on bot caching and intents

--- src/main/java/tech/Astolfo/AstolfoCaffeine/main/cmd/business/Info.java
+++ src/main/java/tech/Astolfo/AstolfoCaffeine/main/cmd/business/Info.java
@@ -3,22 +3,20 @@ package tech.Astolfo.AstolfoCaffeine.main.cmd.business;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
-
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
+import net.dv8tion.jda.api.exceptions.ErrorResponseException;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import tech.Astolfo.AstolfoCaffeine.App;
 import tech.Astolfo.AstolfoCaffeine.main.msg.Logging;
 
+import javax.annotation.Nullable;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
-
-import javax.annotation.Nullable;
 
 import static com.mongodb.client.model.Filters.eq;
 
@@ -37,95 +35,100 @@ public class Info extends Command {
 
     @Override
     protected void execute(CommandEvent e) {
-        Message msg = e.getMessage();
+        try {
+            Message msg = e.getMessage();
 
-        String name = e.getArgs();
-        if (name.equals("")) {
-            e.reply(new Logging().error( "**sowwwwwwwy!!!** u fOwOgot 2 specify the company u wanz to checkzzZZz lmao ;3"));
-            return;
-        }
-
-        Bson filter = eq("name", Pattern.compile(name, Pattern.CASE_INSENSITIVE));
-        Document docs = App.company.find(filter).first();
-
-        if (name.length() > 16) {
-            e.reply(new Logging().error("invawwwwid company nayyyyme ;/"));
-            return;
-        }
-
-        if (docs == null) {
-            e.reply(new Logging().error("AHEAFHBEAUF nuuuu ;( no company matched the query: `"+name+"`"));
-            return;
-        }
-
-        List<Long> members = (List<Long>) docs.get("members");
-        List<Long> admins = (List<Long>) docs.get("admins");
-
-        final MessageEmbed p1;
-        final MessageEmbed p2;
-        final MessageEmbed p3;
-
-        final int page = 1;
-        final int pages;
-
-        if (docs.containsKey("ticker")) {
-            String ticker = docs.getString("ticker");
-            int shares = 0;
-            int shareholders = 0;
-            pages = 3;
-            Document highest = null;
-            for (Document doc : App.stocks.find()) {
-                if (highest == null) {
-                    highest = doc;
-                } else if (highest.getInteger(ticker) < doc.getInteger(ticker)) {
-                    highest = doc;
-                }
-                shares += doc.getInteger(ticker);
-                if (doc.getInteger(ticker) >= 1) {
-                    shareholders += 1;
-                }
+            String name = e.getArgs();
+            if (name.equals("")) {
+                e.reply(new Logging().error("**sowwwwwwwy!!!** u fOwOgot 2 specify the company u wanz to checkzzZZz lmao ;3"));
+                return;
             }
-            assert highest != null;
-            p3 = App.embed()
-                    .setAuthor(docs.getString("name")+" (3/"+pages+")", "https://astolfo.tech", docs.getString("logo"))
-                    .setThumbnail(docs.getString("logo"))
-                    .setDescription("AstolfoEx Stats")
-                    .addField("Majority Shareholder", Objects.requireNonNull(e.getJDA().getUserById(highest.getLong("userID"))).getAsMention(), true)
-                    .addField("Shareholders", String.valueOf(shareholders), true)
-                    .addField("Shares Outstanding", String.valueOf(shares), true)
-                    .build();
-        } else {
-            p3 = null;
-            pages = 2;
-        }
 
-        MessageEmbed page1 = App.embed()
-                .setAuthor(docs.getString("name")+" (1/"+pages+")", "https://astolfo.tech", docs.getString("logo"))
-                .setThumbnail(docs.getString("logo"))
-                .setDescription(docs.getString("description"))
-                .addField("CEO", Objects.requireNonNull(e.getJDA().getUserById(docs.getLong("owner"))).getAsMention(), true)
-                .addField("Employees", String.valueOf(members.size()), true)
-                .addField("Sales", docs.get("xp").toString(), true)
-                .build();
+            Bson filter = eq("name", Pattern.compile(name, Pattern.CASE_INSENSITIVE));
+            Document docs = App.company.find(filter).first();
 
-        MessageEmbed page2 = App.embed()
-                .setAuthor(docs.getString("name")+" (2/"+pages+")", "https://astolfo.tech", docs.getString("logo"))
-                .setThumbnail(docs.getString("logo"))
-                .setDescription("Board of Directors")
-                .addField("Chariman & CEO", Objects.requireNonNull(e.getJDA().getUserById(docs.getLong("owner"))).getAsMention(), true)
-                .addField("Directors", String.valueOf(admins.size()), true)
-                .build();
+            if (name.length() > 16) {
+                e.reply(new Logging().error("invawwwwid company nayyyyme ;/"));
+                return;
+            }
 
-        p1 = page1;
-        p2 = page2;
+            if (docs == null) {
+                e.reply(new Logging().error("AHEAFHBEAUF nuuuu ;( no company matched the query: `" + name + "`"));
+                return;
+            }
 
-        msg.getChannel().sendMessage(page1).queue(
-                m -> {
-                    m.addReaction("◀️").queue();
-                    m.addReaction("▶️").queue();
-                    getReaction(msg, m, page, pages, p1, p2, p3);
+            List<Long> members = (List<Long>) docs.get("members");
+            List<Long> admins = (List<Long>) docs.get("admins");
+
+            final MessageEmbed p1;
+            final MessageEmbed p2;
+            final MessageEmbed p3;
+
+            final int page = 1;
+            final int pages;
+
+            if (docs.containsKey("ticker")) {
+                String ticker = docs.getString("ticker");
+                int shares = 0;
+                int shareholders = 0;
+                pages = 3;
+                Document highest = null;
+                for (Document doc : App.stocks.find()) {
+                    if (highest == null) {
+                        highest = doc;
+                    } else if (highest.getInteger(ticker) < doc.getInteger(ticker)) {
+                        highest = doc;
+                    }
+                    shares += doc.getInteger(ticker);
+                    if (doc.getInteger(ticker) >= 1) {
+                        shareholders += 1;
+                    }
                 }
-        );
+                assert highest != null;
+
+                p3 = App.embed()
+                        .setAuthor(docs.getString("name") + " (3/" + pages + ")", "https://astolfo.tech", docs.getString("logo"))
+                        .setThumbnail(docs.getString("logo"))
+                        .setDescription("AstolfoEx Stats")
+                        .addField("Majority Shareholder", e.getJDA().retrieveUserById(highest.getLong("userID")).complete().getAsMention(), true)
+                        .addField("Shareholders", String.valueOf(shareholders), true)
+                        .addField("Shares Outstanding", String.valueOf(shares), true)
+                        .build();
+            } else {
+                p3 = null;
+                pages = 2;
+            }
+
+            MessageEmbed page1 = App.embed()
+                    .setAuthor(docs.getString("name") + " (1/" + pages + ")", "https://astolfo.tech", docs.getString("logo"))
+                    .setThumbnail(docs.getString("logo"))
+                    .setDescription(docs.getString("description"))
+                    .addField("CEO", e.getJDA().retrieveUserById(docs.getLong("owner")).complete().getAsMention(), true)
+                    .addField("Employees", String.valueOf(members.size()), true)
+                    .addField("Sales", docs.get("xp").toString(), true)
+                    .build();
+
+            MessageEmbed page2 = App.embed()
+                    .setAuthor(docs.getString("name") + " (2/" + pages + ")", "https://astolfo.tech", docs.getString("logo"))
+                    .setThumbnail(docs.getString("logo"))
+                    .setDescription("Board of Directors")
+                    .addField("Chariman & CEO", e.getJDA().retrieveUserById(docs.getLong("owner")).complete().getAsMention(), true)
+                    .addField("Directors", String.valueOf(admins.size()), true)
+                    .build();
+
+            p1 = page1;
+            p2 = page2;
+
+            msg.getChannel().sendMessage(page1).queue(
+                    m -> {
+                        m.addReaction("◀️").queue();
+                        m.addReaction("▶️").queue();
+                        getReaction(msg, m, page, pages, p1, p2, p3);
+                    }
+            );
+        } catch (ErrorResponseException err) {
+            e.replyError("**Sorry!** An unexpected error occured, please contact an Astolfo administrator to repair your company data.");
+        }
     }
 
     private void getReaction(Message msg, Message m, int page, int pages, MessageEmbed page1, MessageEmbed page2, @Nullable MessageEmbed page3) {
