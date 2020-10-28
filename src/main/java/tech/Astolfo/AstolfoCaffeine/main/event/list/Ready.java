f44663dce5fdc664376e4f2dcbe4a56c7311c008
owouwuowo <digitalgpro@gmail.com>
2020-10-28 16:59:39 +0000

    🔨 Updated to support the new cache class instead of the deprecated App.java cache

--- src/main/java/tech/Astolfo/AstolfoCaffeine/main/event/list/Ready.java
+++ src/main/java/tech/Astolfo/AstolfoCaffeine/main/event/list/Ready.java
@@ -1,15 +1,16 @@
 package tech.Astolfo.AstolfoCaffeine.main.event.list;
 
-import java.io.IOException;
-
 import net.dv8tion.jda.api.events.ReadyEvent;
 import tech.Astolfo.AstolfoCaffeine.App;
-import tech.Astolfo.AstolfoCaffeine.main.web.keepOnline;
+import tech.Astolfo.AstolfoCaffeine.main.util.caching.Cache;
+
+import java.io.IOException;
 
 public class Ready {
   public void onReady(ReadyEvent e) throws IOException {
-    keepOnline.innit();
+    //keepOnline.innit();
     App.avatarURL = e.getJDA().getSelfUser().getAvatarUrl();
-    System.out.println(e.getJDA().getSelfUser().getName()+" is online!");
+    Cache.avatarURL = e.getJDA().getSelfUser().getAvatarUrl();
+    System.out.println(e.getJDA().getSelfUser().getName() + " is online!");
   }
 };
--- src/main/java/tech/Astolfo/AstolfoCaffeine/main/event/list/SelfAvatar.java
+++ src/main/java/tech/Astolfo/AstolfoCaffeine/main/event/list/SelfAvatar.java
@@ -2,11 +2,13 @@ package tech.Astolfo.AstolfoCaffeine.main.event.list;
 
 import net.dv8tion.jda.api.events.self.SelfUpdateAvatarEvent;
 import tech.Astolfo.AstolfoCaffeine.App;
+import tech.Astolfo.AstolfoCaffeine.main.util.caching.Cache;
 
 public class SelfAvatar
 {
     public void onChange(SelfUpdateAvatarEvent e)
     {
         App.avatarURL = e.getNewAvatarUrl();
+        Cache.avatarURL = e.getNewAvatarUrl();
     }
 }
