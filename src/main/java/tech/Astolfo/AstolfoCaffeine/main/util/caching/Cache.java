c3286cc95bdf6c974440f8c58a07cc0a08b5b157
owouwuowo <digitalgpro@gmail.com>
2020-10-28 16:58:59 +0000

    ☑ Created a "Cache" class to cache bot-related data

---
+++ src/main/java/tech/Astolfo/AstolfoCaffeine/main/util/caching/Cache.java
@@ -0,0 +1,8 @@
+package tech.Astolfo.AstolfoCaffeine.main.util.caching;
+
+import net.dv8tion.jda.api.JDA;
+
+public class Cache {
+    public static JDA jda;
+    public static String avatarURL;
+}
