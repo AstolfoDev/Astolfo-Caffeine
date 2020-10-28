25eb0c60deaea0d7d57a34be1ff8c621da6a30a2
owouwuowo <digitalgpro@gmail.com>
2020-10-28 16:58:00 +0000

    🔨 Cleaned the switch statement for the Listener#onEvent method

--- src/main/java/tech/Astolfo/AstolfoCaffeine/main/event/Listener.java
+++ src/main/java/tech/Astolfo/AstolfoCaffeine/main/event/Listener.java
@@ -1,7 +1,5 @@
 package tech.Astolfo.AstolfoCaffeine.main.event;
 
-import java.io.IOException;
-
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -11,21 +9,17 @@ import tech.Astolfo.AstolfoCaffeine.main.event.list.Message;
 import tech.Astolfo.AstolfoCaffeine.main.event.list.Ready;
 import tech.Astolfo.AstolfoCaffeine.main.event.list.SelfAvatar;
 
+import java.io.IOException;
+
 public class Listener implements EventListener {
 
     @Override
     public void onEvent(GenericEvent e) {
         try {
-            switch(e.getClass().getName()) {
-              case "net.dv8tion.jda.api.events.ReadyEvent":
-                  new Ready().onReady((ReadyEvent) e);
-                  break;
-              case "net.dv8tion.jda.api.events.self.SelfUpdateAvatarEvent":
-                  new SelfAvatar().onChange((SelfUpdateAvatarEvent) e);
-                  break;
-              case "net.dv8tion.jda.api.events.message.MessageReceivedEvent":
-                  new Message().run((MessageReceivedEvent) e);
-                  break;
+            switch (e.getClass().getName()) {
+                case "net.dv8tion.jda.api.events.ReadyEvent" -> new Ready().onReady((ReadyEvent) e);
+                case "net.dv8tion.jda.api.events.self.SelfUpdateAvatarEvent" -> new SelfAvatar().onChange((SelfUpdateAvatarEvent) e);
+                case "net.dv8tion.jda.api.events.message.MessageReceivedEvent" -> new Message().run((MessageReceivedEvent) e);
             }
         } catch (IOException err) {
           err.printStackTrace();
