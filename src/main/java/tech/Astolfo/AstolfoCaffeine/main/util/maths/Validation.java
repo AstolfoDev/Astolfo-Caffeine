d867a3e9c336752b1856abb696746914af49aeb8
owouwuowo <digitalgpro@gmail.com>
2020-11-06 15:12:11 +0000

    ⚡ Deleted deprecated maths classes as they are no longer needed

--- src/main/java/tech/Astolfo/AstolfoCaffeine/main/util/maths/Validation.java
+++
@@ -1,8 +0,0 @@
-package tech.Astolfo.AstolfoCaffeine.main.util.maths;
-
-public class Validation {
-    // Check if a string can be converted to a number without throwing NumberFormatException
-    public static boolean isNumeric(String str) {
-        return str.matches("-?\\d+(\\.\\d+)?");
-    }
-}
