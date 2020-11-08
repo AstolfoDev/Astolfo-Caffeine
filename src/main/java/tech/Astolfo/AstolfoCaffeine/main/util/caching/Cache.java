10c7f0573eb77294d3ac18df00629e950959125b
owouwuowo <digitalgpro@gmail.com>
2020-11-08 19:51:58 +0000

    📁 Added the "CommandClient" object to the cache

--- src/main/java/tech/Astolfo/AstolfoCaffeine/main/util/caching/Cache.java
+++ src/main/java/tech/Astolfo/AstolfoCaffeine/main/util/caching/Cache.java
@@ -1,8 +1,10 @@
 package tech.Astolfo.AstolfoCaffeine.main.util.caching;
 
+import com.jagrosh.jdautilities.command.CommandClient;
 import net.dv8tion.jda.api.JDA;
 
 public class Cache {
     public static JDA jda;
+    public static CommandClient client;
     public static String avatarURL;
 }
